2188493c2457e102867b5a76a7dedc8d2a7b8765
achulii <96129046+achulii@users.noreply.github.com>
2025-09-05 18:13:19 +0200

    [ADD] business_alerts

---
+++ business_alerts/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Business Alerts",
+    "summary": "Send alerts to n8n",
+    "version": "18.0.1.0.1",
+    "category": "Technical",
+    "author": "bobco GmbH",
+    "maintainers": [
+        "ilyas",
+        "Philip B.",
+        "achulii",
+    ],
+    "license": "OPL-1",
+    "depends": ["mail"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/alert_scenario_views.xml",
+        "views/res_config_settings_views.xml",
+        "data/cron.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
---
+++ business_alerts/models/__init__.py
@@ -0,0 +1,2 @@
+from . import alert_scenario
+from . import res_config_settings
